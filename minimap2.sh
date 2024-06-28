86f107c2dcfb2f3d8fc8d9c94807640ee7f6ec98
Tilly S Scott <125389776+tillysscott@users.noreply.github.com>
2024-06-28 09:08:31 +0100

    Update minimap2.sh

--- minimap2.sh
+++ minimap2.sh
@@ -10,11 +10,30 @@
 #######################################################
 #Use minimap2 to align fastq files from ONT to PacBio reference
 #######################################################
+# set up files and paths
+ref="../../RMod/WG_Hirondellea/Hirondellea_PacBio-pdrnd1.fa"
+reads="Hgigas_ONT.unclassified_kraken_out.fq"
+out="Hgigas_ONT.unclassified_kraken"
+tempdir="temp_minimap2_2"
 
-ref="/uoa/home/r01ms21/sharedscratch/Test_2/RMod/WG_Hirondellea/Hirondellea_PacBio-pdrnd1.fa"
-reads="390and394.unclassified_kraken_out.fq"
-out="390and394.unclassified_kraken_out"
+mkdir $tempdir
 
-
+# load programmes
 module load minimap2
-minimap2 -ax map-ont -t 10 $ref $reads > $out.minimap.sam
+module load samtools
+
+# align ONT against reference genome. 10 threads. BAM output format.
+minimap2 -ax map-ont -t 10 --split-prefix $tempdir $ref $reads | samtools sort -@10 -O bam -o $out.minimap.bam
+## indexes must be split, requires --split-prefix and temporary directory. 
+## Will split index for every ~NUM input target (.: reference) bases [default 4G], change with -I
+
+# index BAM file and get alignment statistics
+samtools index -@8 $out.minimap.bam
+samtools flagstat -@8 $out.minimap.bam > $out.minimap.bam.stats
+
+
+
+########################################################
+# in case of future use:
+## extract aligned reads as new fastq file
+##samtools fastq -@10 -F2308 $out.minimap.bam > $out.mi
