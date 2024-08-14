5c9e532d7aa80996d4bf6d7a48aa7e201ef4bc50
Tilly S Scott <125389776+tillysscott@users.noreply.github.com>
2024-08-14 14:24:02 +0100

    Update kraken2_buildDB.sh

--- kraken2_buildDB.sh
+++ kraken2_buildDB.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem 150G
 #SBATCH --partition=uoa-compute
 
-#module load kraken2
+module load kraken2
 ### Version kraken2/2.1.1 contains a bug in build database step (see slurm-1527761.out and https://github.com/DerrickWood/kraken2/issues/2)
 
 #I have conda installed version 2.1.3
