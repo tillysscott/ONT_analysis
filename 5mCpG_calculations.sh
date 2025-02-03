9ff10a2b65d59880151cdd846658fcdd62cd2e6f
Tilly S Scott <125389776+tillysscott@users.noreply.github.com>
2025-02-03 12:04:30 +0000

    Update 5mCpG_calculations.sh

--- 5mCpG_calculations.sh
+++ 5mCpG_calculations.sh
@@ -10,20 +10,20 @@ grep -v ">" flyegenome450_assembly.fa | awk -F\[Aa] '{print NF-1}' | awk '{SUM+=
 module load seqtk
 seqtk seq -l 0 flyegenome450_assembly.fa | grep -v ">" | grep -o "[Cc][Gg]" | wc -l
 
-#bedtools merge -i 450.pass.methyl.bedMethyl -d 0 -c 10,11,12,13,14,15,16,17,18 -o sum |  awk '{print $0, ($6/$4)*100}' | awk '$13 >= 50' > 450.pass.methyl.bedMethyl.mergedSites_filtered50.bed
-
+# if bedfile is diploid/two stranded, combine orientations, recalculate percentage modified, and filter for percentage modified >=50%
+##bedtools merge -i 450.pass.methyl.bedMethyl -d 0 -c 10,11,12,13,14,15,16,17,18 -o sum |  awk '{print $0, ($6/$4)*100}' | awk '$13 >= 50' > 450.pass.methyl.bedMethyl.mergedSites_filtered50.bed
 
 # count number of CpG sites that are methylated in >= 50% of reads
 awk '$11 >= 50' 450.CG.bedMethyl | wc -l
 
 # count number of CpG sites that are methylated in >= 50% of reads AND have coverage >= 10x
 awk '$11 >= 50' 450.CG.bedMethyl | awk '$5 >= 10' | wc -l
-## result = 514205
+
 
 # count number of CpG sites that are methylated in >= 50% of reads AND have coverage >= 20x
 awk '$11 >= 50' 450.CG.bedMethyl | awk '$5 >= 20' | wc -l
-## result = 141810
+
 
 # count number of CpG sites that are methylated in >= 50% of reads AND have coverage >= 40x
 awk '$11 >= 50' 450.CG.bedMethyl | awk '$5 >= 40' | wc -l
-## result = 63623
+
